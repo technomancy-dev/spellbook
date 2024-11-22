8159892a8190d97ea2bf507c215482c2dbc7ba19
Levi <levi@technomancy.dev>
2024-11-22 05:02:55 +0100

    :sparkles: tailwind, daisy, skeletor

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        skeletor: {
+          primary: "#84FBA2",
+          secondary: "#BD93F9",
+          accent: "#F3E4A2",
+          neutral: "#48445A",
+          "base-100": "#2B2836",
+          info: "#93B4FF",
+          success: "#84FBA2",
+          warning: "#FFB793",
+          error: "#F36A66",
+          "base-content": "#CEDEFF",
+        },
+      },
+    ],
+  },
+};
