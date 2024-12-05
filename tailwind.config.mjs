3a65d29f6bc28f4bd187e225d776fadb80174019
Levi Robertson <levi@technomancy.dev>
2024-12-05 03:21:48 +0100

    :sparkles: landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,17 +4,24 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui"), require('tailwindcss-3d')],
   daisyui: {
+    darkTheme: "skeletor",
     themes: [
-
+      "lofi",
+      "synthwave",
+      "nord",
+      "black",
       {
         skeletor: {
+          ...require("daisyui/src/theming/themes")["black"],
           primary: "#84FBA2",
           secondary: "#BD93F9",
           accent: "#F3E4A2",
           neutral: "#48445A",
           "base-100": "#2B2836",
+          "base-200": "#4e4c59",
+          "base-300": "#757280",
           info: "#93B4FF",
           success: "#84FBA2",
           warning: "#FFB793",
