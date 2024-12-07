1f6ffa03b33fd2cb0636933f7923e993743ef0d9
Levi Robertson <levi@technomancy.dev>
2024-12-07 01:27:09 +0100

    :sparkles: home page changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { Sparkles } from "lucide-react";
 
 const Navigation = ({ children }) => {
   return (
-    <div class="navbar h-12 px-20 bg-base-100">
+    <div class="navbar h-12 px-6 lg:px-20 bg-base-100">
       <div class="flex-1">
         <a href="/" class="font-black flex items-center gap-2 text-xl">
           <Sparkles />
