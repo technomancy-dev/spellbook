a8444cd66e0017c8fe837e613214d11eaac6d2ed
Levi Robertson <levi@technomancy.dev>
2024-12-09 07:07:58 +0100

    :sparkles: astro config sst + node for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import aws from "astro-sst";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import class_to_class_name from "./class-to-classname";
+// import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
+  // Used for local testing of builds.
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  adapter: aws({
+    serverRoutes: [
+      "api/*"     // Directory of API endpoints which require all HTTP methods (weird AWS thing)
+    ]
+  }),
   integrations: [tailwind(), react(), class_to_class_name()],
   vite: {
     alias: {
