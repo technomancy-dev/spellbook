9a6c7ed9a3e934a01354a12696105c2c8123e286
Levi Robertson <levi@technomancy.dev>
2024-12-09 07:11:18 +0100

    :bug: padding on small screens for bottom nav

--- src/layouts/dashboard/dashboard-layout.tsx
+++ src/layouts/dashboard/dashboard-layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }) => {
   return (
     <div class="drawer h-full flex-1 lg:drawer-open border-t border-dashed border-base-200">
       <input id="drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content">
+      <div class="pb-16 lg:pb-0 drawer-content">
         <div class="flex flex-1 h-full animate-fade-in">
         {children}
         </div>
