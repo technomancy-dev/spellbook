44052aa1f22a616fab8bc6f9bdc2800aa60e5752
Levi Robertson <levi@technomancy.dev>
2024-12-09 04:55:39 +0100

    :sparkles: add tailwind3d, animations, and theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,28 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 0.2s ease-in",
+        "fade-out": "fadeOut 0.3s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui"), require('tailwindcss-3d')],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    require("tailwindcss-3d"),
+  ],
   daisyui: {
     darkTheme: "skeletor",
     themes: [
@@ -18,10 +37,10 @@ export default {
           primary: "#84FBA2",
           secondary: "#BD93F9",
           accent: "#F3E4A2",
-          neutral: "#2D2B38",      // Base neutral
-          "base-100": "#2B2836",   // Very close to neutral for cohesion
-          "base-200": "#312F3C",   // Slightly lighter for contrast
-          "base-300": "#34323F",    // A bit lighter still for top layers
+          neutral: "#2D2B38", // Base neutral
+          "base-100": "#2B2836", // Very close to neutral for cohesion
+          "base-200": "#312F3C", // Slightly lighter for contrast
+          "base-300": "#34323F", // A bit lighter still for top layers
           info: "#93B4FF",
           success: "#84FBA2",
           warning: "#FFB793",
